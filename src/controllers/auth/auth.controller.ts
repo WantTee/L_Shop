9a1f3f26f48166d115ed3574357208f92f5c0ece
WantTee <ThePokemon113@gmail.com>
2026-02-26 00:16:02 +0300

    Configured all the controllers

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { AuthService } from "../../services/auth/auth.service";
+
+export class AuthController {
+  static async register(req: Request, res: Response) {
+    try {
+      const user = await AuthService.register(req.body);
+
+      res.cookie("session", user.id, {
+        httpOnly: true,
+        maxAge: 10 * 60 * 1000 // 10 минут
+      });
+
+      res.json({ message: "User registered", user });
+    } catch (e: any) {
+      res.status(400).json({ error: e.message });
+    }
+  }
+
+  static async login(req: Request, res: Response) {
+    try {
+      const user = await AuthService.login(req.body);
+
+      res.cookie("session", user.id, {
+        httpOnly: true,
+        maxAge: 10 * 60 * 1000
+      });
+
+      res.json({ message: "Logged in", user });
+    } catch (e: any) {
+      res.status(400).json({ error: e.message });
+    }
+  }
+}
---
+++ src/controllers/products/products.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { ProductsService } from "../../services/products/products.service";
+
+export class ProductsController {
+  static async getProducts(req: Request, res: Response) {
+    try {
+      const products = await ProductsService.getProducts(req.query);
+      res.json(products);
+    } catch (e: any) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+}
