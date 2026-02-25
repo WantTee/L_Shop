e9020add369e3290dfff09db0bdb4d7f4a89acd8
WantTee <ThePokemon113@gmail.com>
2026-02-26 00:17:53 +0300

    added new one type

---
+++ src/types/bascket.ts
@@ -0,0 +1,12 @@
+import { Product } from "./product";
+
+export interface BasketProduct {
+  count: number;
+  product: Product;
+}
+
+export interface Basket {
+  id: string | number;
+  userId: string | number;
+  basket: BasketProduct[];
+}
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: string | number;
+  name: string;
+  login: string;
+  email: string;
+  phone: string;
+  password: string;
+}
